1efb4c2eb2d8651506416751c11c6322f808ac31
2017940035 <tpfl5424@gmail.com>
2018-11-13 17:44:18 +0900

    11.13

---
+++ 20181113 arraynpointer/anp.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//printArry()
+//입력: 배열
+//출력: 없음
+//부수효과: 화면에 배열의 값을 출력
+//void printArray(int arr[], int len) {
+void printArray(int* arr, int len) { //앞으로 배열을 함수에 전달할 경우 이 문법을 주로 사용한다.
+	for (int index = 0; index < len; index++) {
+		printf("array[%d] = %d\n", index, arr[index]);
+	}
+}
+
+int main() {
+	int a[3] = { 10, 20, 30 };//배열의 선언: 배열이름: a, 배열길이: 3
+	int* ptr;
+	//배열의 이름만 사용하면 그 배열 첫 요소의 포인터
+	ptr = a;//==>ptr = &a[0]
+	printArray(a, 3);
+
+	//a = *ptr; //error WHY? 배열의 이름은 값만 가지고 있다. 즉 rvalue이다. 
+	//10 = a[2]; //eroor  위와 같은 이유
+
+	//printArray(a, 3);
+	return 0;
+}
---
+++ 20181113 selectionSorting/5수.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+/* 함수: printArry()
+입력: 배열
+출력: 없음
+부수효과: 화면에 배열의 값을 출력*/
+void printArray(int* arr, int length) {
+	for (int index = 0; index < length; index++) {
+		printf("array[%d] = %d\n", index, arr[index]);
+	}
+}
+
+/*함수: findMinIndex()
+입력: 배열(배열이름, 배열길이)
+출력: 최소값의 인덱스*/
+int findMinIndex(int* arr, int length) {
+	int minIndex = 0;
+	for (int count = 1; count < length; count++) {
+		if (arr[minIndex] > arr[count])
+			minIndex = count;
+	};
+	return minIndex;
+};
+
+/*함수: swapElement()
+입력: 배열, 두개의 인덱스
+출력: 없음
+부수효과: 배열의 두 인덱스 element가 바뀐다.
+
+*/
+void swapElement(int* arr, int i, int j) {
+	int temp;
+	temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+/*함수: selectSort()
+입력: 배열
+출력: 없음
+부수효과: 배열이 정렬됨*/
+void selectSort(int* arr, int length) {
+	int x;
+	for (x = 1; x < 4; x++) {
+		swapElement(arr, x, min);
+	};
+
+}
+/*함수: findMin()
+입력: 배열(배열이름, 배역길이)
+출력: 배열 최소값*/
+int findMin(int* arr, int length) {
+	int min = arr[0];
+	for (int count = 1; count < length; count++) {
+		if (min > arr[count])
+			min = arr[count];
+	};
+	return min;
+}
+
+int main() {
+	int a[5] = { 30, 35, 27, 15, 40 };
+	printf("minimum value of the array is %d\n", findMin(a, 5));
+	printf("The index of the minimum value is %d\n", findMinIndex(a, 5));
+	swapElement(a, 0, 3);
+	printArray(a, 5);
+	return 0;
+}
